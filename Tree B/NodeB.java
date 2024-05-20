a81c1b1b1ca76a05a3a7532f135a363775df65e6
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2024-05-19 22:44:06 -0300

    Update NodeB.java

--- Tree B/NodeB.java
+++ Tree B/NodeB.java
@@ -31,12 +31,12 @@ class NodeB<T extends Comparable<T>> { private int n;
     this.filhos[i] = f;
   }
                                       
-void preencherRaiz(Object value, NodeB r, NodeB filhoDir) { 
-  this.infos[0] = value;
-  this.filhos[0] = r;
-  this.filhos[1] = filhoDir;
-  this.n = 1; 
-}
+  void preencherRaiz(Object value, NodeB r, NodeB filhoDir) { 
+    this.infos[0] = value;
+    this.filhos[0] = r;
+    this.filhos[1] = filhoDir;
+    this.n = 1; 
+  }
 
 //Insere uma chave e o ponteiro para o filho da direita em um nó 
   void insereChave(T value, NodeB filhoDir) {
@@ -51,45 +51,51 @@ void preencherRaiz(Object value, NodeB r, NodeB filhoDir) {
       this.filhos[k + 1] = this.filhos[k]; 
       this.infos[k] = this.infos[k-1]; 
       k--;
-}
+    }
     
 //insere a chave na posição ideal 
-    this.infos[pos] = value; this.filhos[pos + 1] = filhoDir; this.n++;
-}
-  int buscaBinaria(T value) { int meio, i, f, compara;
-  i = 0;
-  f = this.n - 1;
+    this.infos[pos] = value; 
+    this.filhos[pos + 1] = filhoDir; 
+    this.n++;
+  }
+                                      
+  int buscaBinaria(T value) { 
+    int meio, i, f, compara;
+    i = 0;
+    f = this.n - 1;
   
-  while (i <= f) {
-    meio = (i + f) / 2;
-    compara = value.compareTo((T)this.infos[meio]); 
-    
-    if (compara == 0) {
-      return meio; 
+    while (i <= f) {
+      meio = (i + f) / 2;
+      compara = value.compareTo((T)this.infos[meio]); 
       
-      // Encontrou. Retorna a posição em que a chave está. 
-    } else if (compara < 0) {
-      f = meio - 1; 
-    
-    } else {
-      i = meio + 1; 
-    }
-}
+      if (compara == 0) {
+        return meio; 
+        
+        // Encontrou. Retorna a posição em que a chave está. 
+      } else if (compara < 0) {
+        f = meio - 1; 
+      
+      } else {
+        i = meio + 1; 
+      }
+  }
 
-return i; 
-    //Não encontrou. Retorna a posição do ponteiro para o filho. 
+  return i; //Não encontrou. Retorna a posição do ponteiro para o filho. 
 }
                                       
 public void percorrerEmOrdem() { 
   int i;
-  for (i = 0; i < this.n; i++) { 
+  for (i = 0; i < this.n; i++) {
+    
     if (this.filhos[i] != null) {
       this.filhos[i].percorrerEmOrdem(); 
     }
+    
     System.out.println(this.infos[i]); 
   }
+  
   if (this.filhos[i] != null) { 
       this.filhos[i].percorrerEmOrdem();
+    }
   }
-}
 }
